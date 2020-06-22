e54b37d8d1bd2b00b13a57e1b1a1f05bed6e0e1c
pepps01 <slpepple01@gmail.com>
2020-06-21 21:14:30 -0500

    Added Crawling App to the directory

--- crawlsite/urls.py
+++ crawlsite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crawl/', include('Crawling.urls')),
+    path('', include('Crawling.urls')),
 ]
